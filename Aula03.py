d78bee71b054302efa2f55adf4cc70ae3a9910e0
Carlosgomes1500 <Carlosgomes1500@hotmail.com>
2025-08-12 17:55:52 +0000

    Adiciona Aula03.py

---
+++ Aula03.py
@@ -0,0 +1,12 @@
+print(type("22"))
+print(type(22.3))
+print(type(0))
+print(type(True))
+print(type(False))
+print(type(str(22)))
+int()
+float()
+bool()
+
+nome = imput("Informe su nome:")
+print(type(int(nome)))
