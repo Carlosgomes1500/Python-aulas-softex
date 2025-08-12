a14f2b678f28b42bf0f367ed511a389154762af7
Carlosgomes1500 <Carlosgomes1500@hotmail.com>
2025-08-12 18:46:40 +0000

    Adiciona Aula03.py

---
+++ Aula03_Ex4.py
@@ -0,0 +1,5 @@
+Nome = input("imforme o seu nome:")
+Idade = input("imforme a idade:")
+Curso = input("imforme o curso:")
+
+print(f"Olá, meu nome é {Nome}, tenho {Idade} anos e estou fazendo o curso de {Curso}")
