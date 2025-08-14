a50cac27f1b75c8731425f58987d834dd84c17d4
Carlosgomes1500 <Carlosgomes1500@hotmail.com>
2025-08-14 19:28:28 +0000

    Exercicio

---
+++ Aula04_Ex1.py
@@ -0,0 +1,2 @@
+num = int(input("Digite um numero: "))
+print("par") if num%2==0  else print("impar") 
---
+++ Aula04_Ex2.py
@@ -0,0 +1,11 @@
+nota_aluno = int(input("Informe a primeira nota: "))
+
+
+if nota_aluno >= 7:
+    print("Aluno Aprovado!")
+
+elif nota_aluno >= 5:
+    print("Aluno em Recuperação!")
+
+else:
+     print("Aluno Reprovado!")
