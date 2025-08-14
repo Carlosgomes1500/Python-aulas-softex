53179a2cd69c997555fc9f2eb8c84ffc99abb0bd
Carlosgomes1500 <Carlosgomes1500@hotmail.com>
2025-08-14 18:58:42 +0000

    Aula04

---
+++ Aula04.py
@@ -0,0 +1,15 @@
+'''
+nota01_aluno = int(input("Informe a primeira nota: "))
+nota02_aluno = int(input("Informe a segundo nota: "))
+
+media_aluno = (nota01_aluno + nota02_aluno)/2
+
+if media_aluno >= 6:
+    print("Aluno Aprovado!")
+
+else:
+    print("Aluno Reprovado!")
+'''
+i = int(input())
+for j in range(1, 11):
+    print(i*j)
