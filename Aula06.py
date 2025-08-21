82e1aeaea6f3ffc0d3a447c44c1df214c3d111af
Carlos Henrique Gomes Soares <Carlosgomes1500@hotmail.com>
2025-08-21 19:32:36 +0000

    Coleções

---
+++ Aula06.py
@@ -0,0 +1,12 @@
+lista_vazia = []
+print(type(lista_vazia))
+
+lista_mista = [2, "2", True, [1,2,3], "Ola!", lista_vazia]
+print(lista_mista)
+
+frutas = ["laranja", "melão", "banana"]
+print(frutas[1])
+print(frutas[-1])
+
+for frutas in frutas:
+    print(frutas)
