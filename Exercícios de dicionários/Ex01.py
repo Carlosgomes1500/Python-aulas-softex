44a119a6f27a404f1da07b5434cc54942185c9db
Carlos Henrique Gomes Soares <Carlosgomes1500@hotmail.com>
2025-08-28 19:53:00 +0000

    Lista dicionario

---
+++ Exercícios de dicionários/Ex01.py
@@ -0,0 +1,11 @@
+#1-Crie um dicionário simples
+
+#Crie um dicionário chamado aluno com as chaves: "nome", "idade" e "nota", e preencha com valores fictícios.
+
+aluno = {
+    "nome": "Carlos",
+    "idade": 21,
+    "nota": 9.3
+}
+
+print(aluno)
---
+++ Exercícios de dicionários/Ex04.py
@@ -0,0 +1,14 @@
+"""
+4.
+Removendo elementos
+
+Dado o dicionário:
+
+carro = {"marca": "Ford", "modelo": "Fiesta", "ano": 2010}
+Remova a chave "ano" do dicionário.
+"""
+carro = {
+    "marca": "Ford", 
+    "modelo": "Fiesta", 
+    "ano": 2010
+    }
